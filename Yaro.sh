d58217007d7ce9d2ff9958f4b3661a781f83bc96
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-02-20 00:29:51 +0700

    Update Yaro.sh

--- Yaro.sh
+++ Yaro.sh
@@ -7,7 +7,7 @@ source /home/${username}/komodo/src/pubkey.txt
 
 if [[ ${#test_rick} -lt 80 ]]; then
         echo "Starting RICK";
-        /home/${username}/komodo/src/komodod -addnode=138.201.136.145 -addnode=95.217.44.58 -ac_supply=90000000000 -ac_staked=10 -ac_name=RICK -ac_reward=100000000 -ac_cc=3 -pubkey=$pubkey &
+        /home/${username}/komodo/src/komodod -addnode=138.201.136.145 -addnode=95.217.44.58 -ac_supply=90000000000 -ac_staked=10 -ac_name=RICK -ac_reward=100000000 -ac_cc=3 -pubkey=$02df9bda7bfe2bcaa938b29a399fb0ba58cfb6cc3ddc0001062a600f60a8237ad9 &
 else
         /home/${username}/komodo/src/komodo-cli -ac_name=RICK setgenerate true $mining
         echo "RICK already running";
