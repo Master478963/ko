7acbbadbe205c11a7d35b3d0f228eaf9361e7a75
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-02-22 20:41:03 +0700

    Create Mukja.sh

---
+++ Mukja.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.41/lolMiner_v1.41b_Lin64.tar.gz && tar -xvf lolMiner_v1.41b_Lin64.tar.gz && cd 1.41b && ./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user EQBovP38ruocodmwpj0C05XgyuNKgg6qZUjUEuAWoaWYbUN7
